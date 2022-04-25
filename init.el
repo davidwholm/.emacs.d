a0fde0e56b59a38c31b4e53ce7251c3093b7bd02
David Holmqvist <dicci0308@gmail.com>
2022-04-25 18:50:36 +0200

    Continue adding core functionality

--- init.el
+++ init.el
@@ -10,3 +10,52 @@
       (goto-char (point-max))
       (eval-print-last-sexp)))
   (load bootstrap-file nil 'nomessage))
+
+(straight-use-package 'use-package)
+
+(use-package modus-themes
+  :straight t
+  :custom
+  (modus-themes-mode-line '(accented borderless))
+  (modus-themes-completions '((matches . (extrabold background intense))
+			      (selection . (semibold accented intense))
+			      (popup . (accented))))
+  :init
+  (modus-themes-load-operandi))
+
+(use-package paren
+  :init
+  (show-paren-mode t))
+
+(use-package vertico
+  :straight t
+  :init
+  (vertico-mode))
+
+(use-package consult
+  :straight t
+  :bind
+  ([remap switch-to-buffer] . consult-buffer))
+
+(use-package scroll-bar
+  :custom
+  (scroll-bar-mode nil))
+
+(use-package faces
+  :config
+  (set-face-attribute 'default nil :font "Iosevka"))
+
+(use-package cc-vars
+  :custom
+  (c-basic-offset 4))
+
+(use-package sly
+  :straight t
+  :custom
+  (inferior-lisp-program "sbcl"))
+
+(custom-set-variables
+ '(truncate-lines t)
+ '(ring-bell-function 'ignore)
+ '(menu-bar-mode nil)
+ '(tool-bar-mode nil))
